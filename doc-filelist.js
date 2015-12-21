747a35c7edd92b7703f61ef0a92f7c8074133283
Alinex <info@alinex.de>
2015-12-21 11:12:07 +0100

    Updated documentation

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"src":{"files":["index.coffee"]},"var":{"dirs":{"src":{"dirs":{"style":{"files":["default.css"]}}}}}}};
+var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"src":{"dirs":{"doc":{"files":["test.html","test.md"]}},"files":["index.coffee"]},"var":{"dirs":{"src":{"dirs":{"style":{"files":["default.css"]}}}}}}};
